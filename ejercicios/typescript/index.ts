6005a1d674ef23b41871cc8fbb8b168d26616f3f
juaneins_uio <ballesterosjg@gmail.com>
2021-10-31 16:29:44 -0500

    funciones en typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,10 +1,10 @@
 console.log('Hello typescript');
 
-function add(a: number, b: number) {
+function add(a: number, b: number): number {
   return a + b;
 }
 
-const sum = add(7, 9);
+const sum = add(7, 45);
 
 let muted: boolean = true;
 
@@ -43,3 +43,21 @@ comodin = {};
 // object
 
 let someObject: object = { type: 'Class' };
+
+function createAdder(a: number): (number) => number {
+  return function (b: number) {
+    return a + b;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+console.log(fourPlus6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const juan = fullName('juan', 'ballesteros');
+const pepe = fullName('pepe');
