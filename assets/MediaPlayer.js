e3857246240800a650527807d5e80b4478ab67da
juaneins_uio <ballesterosjg@gmail.com>
2021-10-31 16:52:14 -0500

    Tipando archivo (cambiando a clases)

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,58 +1,53 @@
-function MediaPlayer(config) {
-  this.media = config.el;
-  this.plugins = config.plugins || [];
-  this._initPlugins();
+class MediaPlayer {
+  constructor(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
+  }
+  _initPlugins() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
+      set muted(value) {
+        this.media.muted = value;
+      },
+    };
+
+    //player.muted();
+    this.plugins.forEach((plugin) => {
+      plugin.run(player);
+    });
+  }
+  play() {
+    this.media.play();
+  }
+  pause() {
+    this.media.pause();
+  }
+  togglePlay() {
+    if (this.media.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+  toggleMuted() {
+    if (this.media.muted) {
+      this.unmute();
+    } else {
+      this.mute();
+    }
+  }
+  mute() {
+    this.media.muted = true;
+  }
+  unmute() {
+    this.media.muted = false;
+  }
 }
-
-MediaPlayer.prototype._initPlugins = function () {
-  const player = {
-    play: () => this.play(),
-    pause: () => this.pause(),
-    media: this.media,
-    get muted() {
-      return this.media.muted;
-    },
-    set muted(value) {
-      this.media.muted = value;
-    },
-  };
-
-  //player.muted();
-  this.plugins.forEach((plugin) => {
-    plugin.run(player);
-  });
-};
-
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function () {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
-};
-
-MediaPlayer.prototype.toggleMuted = function () {
-  if (this.media.muted) {
-    this.unmute();
-  } else {
-    this.mute();
-  }
-};
-
-MediaPlayer.prototype.mute = function () {
-  this.media.muted = true;
-};
-
-MediaPlayer.prototype.unmute = function () {
-  this.media.muted = false;
-};
 
 export default MediaPlayer;
