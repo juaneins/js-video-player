5d35af513410066594b63ca7f4a492d19b24aa26
juaneins_uio <ballesterosjg@gmail.com>
2021-10-31 17:20:11 -0500

    Tipando archivo (cambiando a clases)

---
+++ assets/index.ts
@@ -0,0 +1,21 @@
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+
+const button: HTMLElement = document.querySelector('button');
+button.onclick = () => player.togglePlay();
+
+const buttonMute: HTMLElement = document.getElementById('btnMute');
+buttonMute.onclick = () => player.toggleMuted();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch((err) => {
+    console.error(err.message);
+  });
+}
--- assets/plugins/AutoPlay copy.js
+++ assets/plugins/AutoPlay copy.js
@@ -2,7 +2,7 @@ function AutoPlay() {}
 
 AutoPlay.prototype.run = function (player) {
   if (!player.muted) {
-    player.mute = true;
+    player.muted = true;
   }
   player.play();
 };
